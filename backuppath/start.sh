93cdf4647305b5a59dc0c4517dd320213511c45e
Thomas Steinbach <thomas.steinbach@aikq.de>
2015-05-20 19:58:00 +0200

    path bugfixes and more verbose script

--- backuppath/start.sh
+++ backuppath/start.sh
@@ -9,22 +9,24 @@ BASEDIR=$(dirname $0)
 cd "${BASEDIR}"
 
 function backup(){
-  echo "BACK IT UP"
+  echo "Backup: ${SOURCE[@]}"
   backupfile="${BACKUP_DIR}/$(date '+%Y-%m-%d')_${NAME:-backup}.tar.7z"
   if [[ -f "${backupfile}" ]]
-  then rm "${backupfile}"
+    then rm "${backupfile}"
   fi
   tar cf - "${SOURCE[@]}" | 7zr a -si "${BACKUP_DIR}/$(date '+%Y-%m-%d')_${NAME:-backup}.tar.7z"
   chown -R user1000:user1000 "${BACKUP_DIR}"
+  echo "Backup created in $backupfile"
 }
 
 function restore(){
-  echo "RE STOREs"
-  if [[ ! -z $(ls "${BACKUP_DIR}/${SOURCE}") ]]
+  echo "Restore backup ${BACKUP_DIR}"
+  if [[ ! -z $(ls "${BACKUP_DIR}"/${SOURCE} 2>/dev/null) ]]
   then
-    7zr e "${BACKUP_DIR}/${SOURCE}" -o/tmp/backup
-    # remote previous directories
-    echo "remove: "
+    echo "Backup archive is: $(ls ${BACKUP_DIR}/${SOURCE})"
+    7zr e "${BACKUP_DIR}"/${SOURCE} -o/tmp/backup
+    # remove previous directories
+    echo "Clear and restore paths contained in archive: "
     tar tf /tmp/backup/*.tar | python tarpaths.py
     rm -rf $(tar tf /tmp/backup/*.tar | python tarpaths.py)
     tar --same-owner -C / -pxf /tmp/backup/*.tar
